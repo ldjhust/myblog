69b199ce343acec057bba37dc421b3618e1d391d
leo <lidongjie2012@hust.edu.cn>
2016-11-27 17:59:44 +0800

    Init

--- hello.py
+++ hello.py
@@ -1,5 +1,30 @@
 #coding=utf-8
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>/')
+def user(name):
+    config = {
+        'name': name
+    }
+    return render_template('user.html', **config)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9000)
