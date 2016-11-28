0c379ece30174c62ab0e0a8977a49e449b258cf8
leo <lidongjie2012@hust.edu.cn>
2016-11-28 18:26:44 +0800

    Init

--- hello.py
+++ hello.py
@@ -1,18 +1,27 @@
 #coding=utf-8
 
 from datetime import datetime
+import os
 
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_script import Manager
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 from forms import NameForm
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 Bootstrap(app)
 Moment(app)
+db = SQLAlchemy(app)
+manager = Manager(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -45,5 +54,24 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+class User(db.Model):
+    __tablename__ =  'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9000)
+    # app.run(host='0.0.0.0', port=9000)
+    manager.run()
