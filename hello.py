beea078567c2b25d1948650a16765dc4f64fe272
leo <lidongjie2012@hust.edu.cn>
2016-11-28 11:04:38 +0800

    Init

--- hello.py
+++ hello.py
@@ -1,22 +1,30 @@
 #coding=utf-8
 
+from datetime import datetime
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'hard to guess string'
 Bootstrap(app)
+Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    context = {
+        'current_time': datetime.utcnow()
+    }
+    return render_template('index.html', **context)
 
 @app.route('/user/<name>/')
 def user(name):
-    config = {
+    context = {
         'name': name
     }
-    return render_template('user.html', **config)
+    return render_template('user.html', **context)
 
 @app.errorhandler(404)
 def page_not_found(e):
